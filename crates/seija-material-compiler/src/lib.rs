6ae25d08c9a2d05b211eeddb92fa4a62e3aac6f1
yangdao <458424869@qq.com>
2021-11-30 20:52:31 +0800

    material compiler

--- crates/seija-material-compiler/src/lib.rs
+++ crates/seija-material-compiler/src/lib.rs
@@ -1,5 +1,18 @@
-pub struct CompilerEnv {
-    shader_folders:Vec<String>
+use std::path::{Path,PathBuf};
+#[derive(Default)]
+pub struct CompilerConfig {
+    shader_folders:Vec<String>,
+    material_folders:Vec<String>
+}
+
+impl CompilerConfig {
+    pub fn add_shader_path(&mut self,path:&str) {
+        self.shader_folders.push(path.to_string());
+    }
+
+    pub fn add_material_path(&mut self,path:&str) {
+        self.material_folders.push(path.to_string());
+    }
 }
 
 
@@ -7,13 +20,23 @@ pub struct MaterialCompiler {
 
 }
 
+impl MaterialCompiler {
+    pub fn start(&self,env:&CompilerConfig) {
+        for f in env.material_folders.iter() {
+            self.compile_material_folder(f.as_str());
+        }
+    }
+
+    fn compile_material_folder(&self,folder:&str) {
+        
+    }
+}
 
 #[test]
 fn test_shaderc() {
-    use shaderc::*;
-    let compile = Compiler::new().unwrap();
-    let opts = CompileOptions::new().unwrap();
-    
-    
-    
+    let mut config = CompilerConfig::default();
+    let cur_dir = std::env::current_dir().unwrap();
+    let sp = cur_dir.join("../shaders");
+    config.add_shader_path(sp.to_str().unwrap());
+    println!("cur dir:{:?}",cur_dir);
 }
