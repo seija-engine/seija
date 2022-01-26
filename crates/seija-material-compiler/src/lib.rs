d888d4e1e6996dd8f423c2e045d5feb1783145d1
yangdao <458424869@qq.com>
2022-01-26 17:20:20 +0800

    material  compiler

--- crates/seija-material-compiler/src/lib.rs
+++ crates/seija-material-compiler/src/lib.rs
@@ -1,9 +1,36 @@
+use std::path::{PathBuf, Path};
+
 use glsl_pkg::PackageManager;
-
 use crate::backend::SeijaShaderBackend;
-
 mod backend;
 
+pub struct MaterialCompiler {
+   folders:Vec<PathBuf>,
+   pkg_mgr:PackageManager,
+   backend:SeijaShaderBackend
+}
+
+impl MaterialCompiler {
+   pub fn new() -> Self {
+      MaterialCompiler { folders: vec![], pkg_mgr: PackageManager::new(),backend:SeijaShaderBackend::new() }
+   }
+
+   pub fn add_shader_dir(&mut self,dir:&str) {
+      self.pkg_mgr.add_dir(dir)
+   }
+
+   pub fn set_shader_out(&mut self,dir:&str) {
+      self.pkg_mgr.set_out_path(dir);
+   }
+
+   pub fn add_mat_search_path(&mut self,dir:&str) {
+      self.folders.push(PathBuf::from(dir));
+   }
+
+   pub fn run(&mut self) {
+
+   }
+}
 
 #[test]
 fn test_fn() {
@@ -12,4 +39,5 @@ fn test_fn() {
    pkg_mgr.add_dir("../shaders");
    pkg_mgr.set_out_path("../.shader_out");
    pkg_mgr.compile("core", "color", vec![], &backend);
+   
 }
