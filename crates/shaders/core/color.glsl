cfdf594b6299386c057077a3f7ad5c4bd9e1df7f
yangdao <458424869@qq.com>
2022-03-21 16:46:25 +0800

    test shader

--- crates/shaders/core/color.glsl
+++ crates/shaders/core/color.glsl
@@ -2,14 +2,12 @@ struct VSOutput {
   vec3 color;
 };
 
-VSOutput vs_main() {
-  VSOutput o;
+void vs_main() {
   vec4 pos = getTransform() * vec4(vert_position, 1.0);
   gl_Position = getCameraProjView() * pos;
-  return o;
 }
 
-vec4 fs_main(VSOutput vo) {
+vec4 fs_main() {
     vec4 color = vec4(1,0,0,1);
     color.a = 0.5;
     return color;
