2dd09bd268284c50cf54227a6e8e2adaaff1574f
yangdao <458424869@qq.com>
2022-07-15 20:58:36 +0800

    query

--- crates/seija-render/src/query/camera_query.rs
+++ crates/seija-render/src/query/camera_query.rs
@@ -1,30 +1,70 @@
+use std::collections::{HashMap, HashSet};
+
 use bevy_ecs::{prelude::{World, Entity, Query, ResMut, Res, Added, RemovedComponents}};
-use seija_asset::Handle;
-use seija_core::info::EInfo;
-use seija_transform::Transform;
+use parking_lot::RwLock;
+
 
 use crate::{material::{MaterialStorage, Material}, camera::camera::Camera};
 
-use super::{view_list::{ViewList, ViewEntity}, QuerySystem};
+use super::{view_list::{ViewList, ViewEntity}, QuerySystem, ViewQuery};
 
 const CAMERA_TYPE:u32 = 1u32;
 
-pub struct CameraQuery {
-    camera_entity:Entity
+#[derive(Default)]
+pub struct CameraQueryMap {
+    pub map:HashMap<u64,usize>
 }
 
-pub(crate) fn camera_query_check_add(system:ResMut<QuerySystem>,add_cameras:Query<Entity,Added<Camera>>,remove_cameras:RemovedComponents<Camera>) {
-    if add_cameras.is_empty() && remove_cameras.iter().next().is_none() { return ; }
-    for add_camera in add_cameras.iter() {
-        
+pub(crate) fn camera_query_check_add(mut camera_query_map:ResMut<CameraQueryMap>,
+                                     mut system:ResMut<QuerySystem>,
+                                     add_cameras:Query<Entity,Added<Camera>>,
+                                     remove_cameras:RemovedComponents<Camera>) {
+    let  has_remove = remove_cameras.iter().next().is_some();
+    if add_cameras.is_empty() && !has_remove { return ; }
+    for entity in add_cameras.iter() {
+        let eid = entity.to_bits();
+        if !camera_query_map.map.contains_key(&eid) {
+            system.querys.push(RwLock::new(ViewQuery::new(CAMERA_TYPE, eid)));
+            camera_query_map.map.insert(eid, system.querys.len());
+        }
+    }
+    if has_remove {
+        let mut rm_set:HashSet<u64> = HashSet::default();
+        for rm in remove_cameras.iter() {
+            let rm_eid = rm.to_bits();
+            rm_set.insert(rm_eid);
+            camera_query_map.map.remove(&rm_eid);
+        }
+
+        if rm_set.len() > 0 {
+            let mut i = 0;
+            while i < system.querys.len() {
+                if rm_set.contains(&system.querys[i].read().key) {
+                  system.querys.remove(i);
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
+        for (index,view_query) in system.querys.iter().enumerate() {
+            let query_ref = view_query.read();
+            camera_query_map.map.insert(query_ref.key, index);
+        }
     }
 }
 
-pub(crate) fn camera_query_update(system:ResMut<QuerySystem>) { 
+pub(crate) fn camera_query_update(camera_map:Res<CameraQueryMap>,system:Res<QuerySystem>) {
+    for (eid,index) in camera_map.map.iter() {
+       let view_query = &system.querys[*index];
+       if view_query.read().typ == CAMERA_TYPE {
+        
+       }
+    }
 }
 
 
-
+/* 
 impl CameraQuery {
     fn on_query(&self,world:&mut World,list:&mut ViewList) {
         list.clear();
@@ -50,4 +90,4 @@ impl CameraQuery {
         }
         list.sort();   
     }
-}
+}*/
--- crates/seija-render/src/query/system.rs
+++ crates/seija-render/src/query/system.rs
@@ -1,17 +1,27 @@
-use std::sync::{RwLock};
 use bevy_ecs::{prelude::{World,Entity}};
+use parking_lot::RwLock;
 use super::view_list::ViewList;
 
-pub struct Query {
-    typ:u32,
-    key:u32,
+pub struct ViewQuery {
+    pub typ:u32,
+    pub key:u64,
     list:ViewList
 }
 
+impl ViewQuery {
+    pub fn new(typ:u32,key:u64) -> Self {
+        ViewQuery { typ, key, list:ViewList::default() }
+    }
+}
 
 #[derive(Default)]
 pub struct QuerySystem {
-    //这里可以考虑换成UnsafeCell
-    pub querys:Vec<RwLock<Query>>
+    pub querys:Vec<RwLock<ViewQuery>>
+}
+
+impl QuerySystem {
+    pub fn remove_by_key(&mut self,key:u64) {
+       
+    }
 }
 
