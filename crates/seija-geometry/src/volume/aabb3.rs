3d3c65f583ad5230db08737f2ea0c44d5a64bbf4
yangdao <458424869@qq.com>
2022-09-06 11:50:14 +0800

    八叉树

--- crates/seija-geometry/src/volume/aabb3.rs
+++ crates/seija-geometry/src/volume/aabb3.rs
@@ -3,7 +3,7 @@ use glam::Vec3;
 use crate::{traits::Contains, bound::{PlaneBound, Relation}};
 
 use super::aabb::IAABB;
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 pub struct AABB3 {
     pub min: Vec3,
     pub max: Vec3,
--- crates/seija-render/src/query/scene_octree.rs
+++ crates/seija-render/src/query/scene_octree.rs
@@ -1,11 +1,11 @@
 use bevy_ecs::prelude::Entity;
 use glam::Vec3;
-use seija_geometry::{octree::{Octree, OctantId}, volume::{AABB3, IAABB},Contains};
+use seija_geometry::{ volume::{AABB3, IAABB},Contains};
 
 #[derive(Debug)]
 pub struct SceneElement {
-    entity:Option<Entity>,
-    aabb:AABB3
+    pub entity:Option<Entity>,
+    pub aabb:AABB3
 }
 
 
@@ -14,20 +14,27 @@ type NodeId = usize;
 #[derive(Debug)]
 pub struct SceneOctreeNode {
     pub aabb:AABB3,
-    parent:Option<NodeId>,
-    pub children:Vec<NodeId>,
+    pub parent:Option<NodeId>,
+    pub child_start:NodeId,
     pub objects:Vec<SceneElement>
 }
 
 impl SceneOctreeNode {
     pub fn new(parent:Option<NodeId>,aabb:AABB3) -> Self {
-        SceneOctreeNode {parent,children:vec![],objects:vec![],aabb }
+        SceneOctreeNode {parent,child_start:0,objects:vec![],aabb }
     }
 
-   
+    pub fn remove(&mut self,entity:Entity) -> bool {
+        if let Some(idx) = self.objects.iter().position(|v| v.entity == Some(entity)) {
+            self.objects.remove(idx);
+            return true;
+        }
+        false
+    }
 }
 #[derive(Debug)]
 pub struct SceneOctree {
+    max_depth:usize,
     root:NodeId,
     nodes:Vec<SceneOctreeNode>
 }
@@ -35,31 +42,63 @@ pub struct SceneOctree {
 impl SceneOctree {
     pub fn new(min:Vec3,max:Vec3) -> Self {
         let aabb = AABB3::new(min, max);
-        let node = SceneOctreeNode::new(None,aabb);
-        SceneOctree { root: 0, nodes: vec![node] }
+        let mut node = SceneOctreeNode::new(None,aabb);
+        node.child_start = 0;
+        SceneOctree { max_depth:4,root: 0, nodes: vec![node] }
     }
 
-    pub fn add(&mut self,entity:Entity,aabb:AABB3) {
-        if self.node_add(self.root,entity, aabb) {
-
-            //grow
-            todo!()
+    pub fn add(&mut self,entity:Entity,aabb:AABB3) -> NodeId {
+        loop {
+            if let Some(id) = self.node_add(self.root,entity, &aabb,self.max_depth) {
+                return id
+            } else {
+                self.grow()
+            }
         }
     }
 
-    fn node_add(&mut self,id:NodeId,entity:Entity,aabb:AABB3) -> bool {
-        if !self.nodes[id].aabb.contains(&aabb) {
-            return false;
+    pub fn remove_bynode(&mut self,id:NodeId,entity:Entity) -> bool {
+        if let Some(node) = self.get_node_mut(id) {
+           return node.remove(entity);
+        }
+        false
+    }
+
+    pub fn get_node(&self,id:NodeId) -> Option<&SceneOctreeNode> {
+        self.nodes.get(id)
+    }
+
+    pub fn get_node_mut(&mut self,id:NodeId) -> Option<&mut SceneOctreeNode> {
+        self.nodes.get_mut(id)
+    }
+
+    fn grow(&mut self) {
+         //grow
+         unimplemented!()
+    }
+
+    fn node_add(&mut self,id:NodeId,entity:Entity,aabb:&AABB3,depth:usize) -> Option<NodeId> {
+        println!("add element: {:?} in {:?}",&self.nodes[id].aabb,&aabb);
+        if !self.nodes[id].aabb.contains(aabb) {
+            return None;
         }
         self.check_split(id);
         let offset_index = self.best_fit_child(id, aabb.center());
-        let fit_index = self.nodes[id].children[0] + offset_index;
-        if self.nodes[fit_index].aabb.contains(&aabb) {
-
+        let fit_index = self.nodes[id].child_start + offset_index;
+        if depth <= 0 {
+            let elem = SceneElement {entity:Some(entity),aabb:aabb.clone() } ;
+            self.nodes[id].objects.push(elem);
+            Some(id)
         } else {
-            todo!()
+            if let Some(node_id) = self.node_add(fit_index, entity, aabb, depth - 1) {
+                Some(node_id)
+            } else {
+                let elem = SceneElement {entity:Some(entity),aabb:aabb.clone() } ;
+                self.nodes[id].objects.push(elem);
+                Some(id)
+            }
         }
-        true
+       
     }
 
     fn best_fit_child(&self,id:NodeId,center:Vec3) -> usize {
@@ -71,7 +110,7 @@ impl SceneOctree {
     }
 
     fn check_split(&mut self,id:NodeId) {
-        if self.nodes[id].children.len() > 0 { return ; }
+        if self.nodes[id].child_start > 0 { return ; }
         //从上到下从左到右从后到前12个点
         let min = self.nodes[id].aabb.min;
         let max = self.nodes[id].aabb.max;
@@ -86,7 +125,7 @@ impl SceneOctree {
         let node6 = SceneOctreeNode::new(Some(id), AABB3::new(Vec3::new(half_x, min.y, min.z), Vec3::new(max.x, half_y, half_z)));
         let node7 = SceneOctreeNode::new(Some(id), AABB3::new(Vec3::new(min.x, min.y, half_z), Vec3::new(half_x, half_y, max.z)));
         let node8 = SceneOctreeNode::new(Some(id), AABB3::new(Vec3::new(half_x, min.y,half_z), Vec3::new(max.x, half_y, max.z)));
-        let start_index = self.nodes.len();
+        self.nodes[id].child_start = self.nodes.len();
         self.nodes.push(node1);
         self.nodes.push(node2);
         self.nodes.push(node3);
@@ -95,12 +134,21 @@ impl SceneOctree {
         self.nodes.push(node6);
         self.nodes.push(node7);
         self.nodes.push(node8);
-        for idx in start_index..(start_index + 8) {
-            self.nodes[id].children.push(idx);
-        }
     }
 
-   
+    
+    pub fn update(&mut self,node_id:NodeId,entity:Entity,new_aabb:AABB3) -> bool {
+        let cur_node = &mut self.nodes[node_id];
+        if let Some(index)  = cur_node.objects.iter().position(|v| v.entity == Some(entity)) {
+            if cur_node.aabb.contains(&new_aabb) {
+                cur_node.objects[index].aabb = new_aabb;
+                return true;
+            }
+
+            unimplemented!() // ??
+        }
+        false
+    }
 }
 
 #[test]
@@ -108,6 +156,14 @@ fn test_tree() {
     let v3_100 = Vec3::new(100f32, 100f32, 100f32);
     let mut scene_tree = SceneOctree::new(-v3_100,v3_100);
     scene_tree.check_split(scene_tree.root);
-    dbg!(scene_tree);
    
+  
+    let aabb0 = AABB3::new(Vec3::new(1f32, 1f32, 1f32), Vec3::new(11f32, 11f32, 11f32));
+    let e0 = Entity::from_raw(0);
+
+    let add_id = scene_tree.add(e0, aabb0);
+    println!("addid:{}",add_id);
+    let b = scene_tree.remove_bynode(add_id, e0);
+    println!("{}",b);
+    //dbg!(&scene_tree);
 }
