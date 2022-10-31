7d05f7d4e4001da57e28ab57517f01d8708e76af
yangdao <458424869@qq.com>
2022-10-31 20:37:04 +0800

    frp doc

--- doc/FRPRender.clj
+++ doc/FRPRender.clj
@@ -2,7 +2,7 @@
 
 (defn node-camera [uname] (node CAMERA_NODE uname))
 
-(frp-comp base-3d-common []
+(def-frp-comp base-3d-common []
   (uniform  "ObjectBuffer")
   (uniform  "CameraBuffer")
   (uniform  "LightBuffer" )
@@ -14,26 +14,36 @@
 
 
 
-(frp-comp start []
-  (cond-comp dynEnableBase3D '(base-3d-common ))
+(def-frp-comp start []
+  (if-comp dynEnableBase3D '(base-3d-common ))
   (add-render-path foward-path-start)
 )
 
-(frp-comp foward-path-start [{:targetView window-texture :camera-id  camera-id :camera-query camera-query}]
+(def-frp-comp foward-path-start [{:targetView window-texture :camera-id  camera-id :camera-query camera-query}]
    (let [depth-texture (texture {:format "Depth32Float"})]
       (node WINDOW_SIZE depth-texture)
-      (cond-comp dynIsHdr '(hdr-draw depth-texture window-texture camera-id camera-query)
+      (if-comp dynIsHdr '(hdr-draw depth-texture window-texture camera-id camera-query)
                           '(normal-draw depth-texture window-texture camera-id camera-query))
    )
 )
 
-(frp-comp hdr-draw [depth-texture window-texture camera-id camera-query]
+(def-frp-comp hdr-draw [depth-texture window-texture camera-id camera-query]
+   (add-post-effect camera_id "tonemap")
    (let [hdr-texture (texture {:format "Rgba16Float"})] 
       (node DRAW_PASS camera_query camera_id [hdr-texture] depth-texture "Foward")
       (node POST_STACK camera_id hdr-texture window-texture)
    )
 )
 
-(frp-comp normal-draw [depth-texture window-texture camera-id camera-query]
+(def-frp-comp normal-draw [depth-texture window-texture camera-id camera-query]
+   (if-comp dynHasPostEffect '(normal-draw-post-effect depth-texture window-texture camera-id camera-query)
+                               (frp-comp #(node DRAW_PASS camera_query camera_id [window-texture] depth-texture "Foward"))
+   )
+)
 
+(def-frp-comp normal-draw-post-effect [depth-texture window-texture camera-id camera-query]
+   (let [cache-texture (texture {:format (get-format  window-texture)})]
+      (node DRAW_PASS camera_query camera_id [cache-texture] depth-texture "Foward")
+      (node POST_STACK camera_id cache-texture window-texture)
+   )
 )
--- doc/最终基于FRP的渲染DSL.md
+++ doc/最终基于FRP的渲染DSL.md
@@ -1,7 +1,7 @@
 ## FRP组件  
 ### 概述  
 FRP组件是FRP系统的基础。FRP组件以函数的方式声明，但他并不是传统的输入-输出的函数。 
-`frp-comp`其实是一个Rust端的FRPNode的Builder。通过`frp-comp`的定义最终会构建出一个FRPNode的动态组件树。  
+`def-frp-comp`其实是一个Rust端的FRPNode的Builder。通过`def-frp-comp`的定义最终会构建出一个FRPNode的动态组件树。  
 FRP系统里的基础概念`Event`,`Behavior`这里略过，参考`reflex-frp`。  
 
 
@@ -14,7 +14,7 @@ FRP组件的生成，就是为了生成一些基础元素到FRPNode上。
 
 FRP组件激活和取消激活都会有对应的响应函数。响应函数同时会调用组件下所有元素的激活和取消激活。    
 ```Clojure
-(frp-comp base-3d-common []
+(def-frp-comp base-3d-common []
   (uniform  "ObjectBuffer")
   (uniform  "CameraBuffer")
   (uniform  "LightBuffer" )
@@ -23,14 +23,14 @@ FRP组件激活和取消激活都会有对应的响应函数。响应函数同�
 ```
 
 ### FRP的组件动态处理  
-1. <b>cond-comp</b>开关组件  
+1. <b>if-comp</b>开关组件  
      第一个参数接收一个`Dynamic Bool`表示组件的开关.  
      第二个参数接收组件构建函数。  
      根据外部`Dynamic Bool`的变化为true这个组件会被激活,为false会被关闭
      第三个参数可选`Dynamic Bool`为false的时候激活，为true的时候关闭
 ```Clojure
-(frp-comp start []
-  (cond-comp (const-dyn true) '(base-3d-common))
+(def-frp-comp start []
+  (if-comp (const-dyn true) '(base-3d-common))
 )
 ```
 
