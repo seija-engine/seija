8ebc7b63d27ce167528ce180cb1e90c2aa3dc751
yangdao <458424869@qq.com>
2023-02-10 19:00:28 +0800

    ui layout

--- crates/seija-ui/src/layout/measure.rs
+++ crates/seija-ui/src/layout/measure.rs
@@ -1,5 +1,5 @@
 use bevy_ecs::prelude::Entity;
-use seija_core::math::Vec2;
+use seija_core::math::{Vec2, Vec3};
 use super::{types::{LayoutElement, TypeElement}, system::LayoutParams, comps::{StackLayout, Orientation}};
 
 pub fn measure_layout_element(entity:Entity,request_size:Vec2,element:&LayoutElement,params:&LayoutParams) -> Vec2 {
@@ -19,48 +19,75 @@ pub fn measure_layout_element(entity:Entity,request_size:Vec2,element:&LayoutEle
     measure_size
 }
 
-//View Box
+//View Box 自适配模式下会被最大的子节点撑开
 fn measure_viewbox_layout(entity:Entity,request_size:Vec2,element:&LayoutElement,params:&LayoutParams) -> Vec2 {
-    let mut content_size = element.common.get_content_size(request_size, params.rect2ds.get(entity).ok());
-    let inner_size = Vec2::new(content_size.x - element.common.padding.horizontal(),content_size.y - element.common.padding.vertical());
+    let (fixed_size,free_inner_size) = element.common.get_size_info(request_size, params.rect2ds.get(entity).ok());
+    let mut ret_size = fixed_size;
     if let Ok(children) = params.childrens.get(entity) {
-        for child_entity in children.iter() {
-           if let Ok(child_elem) = params.elems.get(*child_entity) {
-              let child_size = measure_layout_element(*child_entity, inner_size, child_elem, params);
-              if child_size.x > content_size.x {
-                content_size.x = child_size.x;
-              }
-              if child_size.y > content_size.y {
-                content_size.y = child_size.y;
-              }
-           }
-        }
-
+        let mut is_dirty = false;
         for child_entity in children.iter() {
             if let Ok(child_elem) = params.elems.get(*child_entity) {
-                measure_layout_element(*child_entity, content_size, child_elem, params);
+                let child_size = measure_layout_element(*child_entity, free_inner_size, child_elem, params);
+                if fixed_size.x < 0f32 && child_size.x > ret_size.x {
+                    ret_size.x = child_size.x;
+                    is_dirty = true;
+                }
+                if fixed_size.y < 0f32 && child_size.y > ret_size.y {
+                    ret_size.y = child_size.y;
+                    is_dirty = true;
+                }
+            }
+        }
+        if is_dirty {
+            for child_entity in children.iter() {
+                if let Ok(child_elem) = params.elems.get(*child_entity) {
+                    measure_layout_element(*child_entity, ret_size, child_elem, params);
+                }
             }
         }
     }
-    Vec2::ZERO
+
+    ret_size
 }
 
 ////Stack System
 fn measure_stack_layout(entity:Entity,stack:&StackLayout,request_size:Vec2,element:&LayoutElement,params:&LayoutParams) -> Vec2 {
-    let mut ret_size = request_size;
-    //let content_size:Vec2 = element.common.get_content_size(request_size, params.rect2ds.get(entity).ok());
-    //let inner_size:Vec2 = Vec2::new(content_size.x - element.common.padding.horizontal(),
-    //                               content_size.y - element.common.padding.vertical());
-    
+    let (fixed_size,free_inner_size) = element.common.get_size_info(request_size, params.rect2ds.get(entity).ok());
+    let mut ret_size = fixed_size;
     match stack.orientation {
-           Orientation::Horizontal => ret_size.x = element.common.padding.left,
-           Orientation::Vertical => ret_size.y = element.common.padding.top
-    }
-
-    if let Ok(children) = params.childrens.get(entity) {
-        for child_entity in children.iter() {
-
+        Orientation::Horizontal => {
+            ret_size.x = element.common.padding.horizontal();
+        },
+        Orientation::Vertical => { 
+            ret_size.y = element.common.padding.vertical();
         }
     }
-    Vec2::ZERO
+
+    let childs = params.childrens.get(entity);
+    if let Ok(childs) = childs {
+        for child_entity in childs.iter() {
+            if let Ok(child_element) = params.elems.get(entity) {
+                let child_size = measure_layout_element(*child_entity, free_inner_size, child_element, params);
+                match stack.orientation {
+                    Orientation::Horizontal => {
+                        ret_size.x += child_size.x;
+                    },
+                    Orientation::Vertical => { 
+                        ret_size.y += child_size.y;
+                    }
+                }
+            }
+        }
+    }
+
+    let free_x = request_size.x - element.common.margin.horizontal();
+    if ret_size.x > free_x {
+        ret_size.x = free_x;
+    }
+    let free_y = request_size.y - element.common.margin.vertical();
+    if ret_size.y > free_y {
+        ret_size.y = free_y;
+    }
+   
+    ret_size
 }
--- crates/seija-ui/src/layout/types.rs
+++ crates/seija-ui/src/layout/types.rs
@@ -1,87 +1,118 @@
-use bevy_ecs::{prelude::Component};
+use bevy_ecs::prelude::Component;
 use seija_core::math::Vec2;
 
-use crate::{types::Thickness, components::rect2d::Rect2D};
+use crate::{components::rect2d::Rect2D, types::Thickness};
 
-use super::comps::{StackLayout, Orientation};
+use super::comps::{Orientation, StackLayout};
 
-#[derive(PartialEq,Eq,Clone,Copy,Debug)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum LayoutAlignment {
     Start = 0,
-	Center = 1,
-	End = 2,
-	Stretch = 3
+    Center = 1,
+    End = 2,
+    Stretch = 3,
 }
 
 #[derive(Component)]
 pub struct CommonView {
-    pub size:Vec2,
+    pub size: Vec2,
     pub margin: Thickness,
     pub padding: Thickness,
     pub hor: LayoutAlignment,
     pub ver: LayoutAlignment,
-    pub use_rect_size:bool,
+    pub use_rect_size: bool,
 }
 
 impl Default for CommonView {
     fn default() -> Self {
         CommonView {
-            size:Vec2::new(-1f32, -1f32),
-            margin:Thickness::default(),
-            padding:Thickness::default(),
-            hor:LayoutAlignment::Stretch,
-            ver:LayoutAlignment::Stretch,
-            use_rect_size:false
+            size: Vec2::new(-1f32, -1f32),
+            margin: Thickness::default(),
+            padding: Thickness::default(),
+            hor: LayoutAlignment::Stretch,
+            ver: LayoutAlignment::Stretch,
+            use_rect_size: false,
         }
     }
 }
 
 impl CommonView {
-    pub fn get_content_size<'w,'s>(&self,request_size:Vec2,rect2d:Option<&Rect2D>) -> Vec2 {
+    pub fn get_content_size<'w, 's>(&self, request_size: Vec2, rect2d: Option<&Rect2D>) -> Vec2 {
         let mut ret_size = self.get_size(rect2d);
-        if ret_size.x < 0f32 && self.hor == LayoutAlignment::Stretch {
+        if ret_size.x < 0f32 || self.hor == LayoutAlignment::Stretch {
             ret_size.x = request_size.x - self.margin.horizontal();
         }
-        if ret_size.y < 0f32 && self.ver == LayoutAlignment::Stretch {
+        if ret_size.y < 0f32 || self.ver == LayoutAlignment::Stretch {
             ret_size.y = request_size.y - self.margin.vertical();
         }
         ret_size
     }
 
-    pub fn get_size(&self,rect2d:Option<&Rect2D>) -> Vec2 {
+    pub fn get_size(&self, rect2d: Option<&Rect2D>) -> Vec2 {
         if self.use_rect_size {
-            rect2d.map(|rect| Vec2::new(rect.width,rect.height)).unwrap_or(Vec2::ZERO)
+            rect2d
+                .map(|rect| Vec2::new(rect.width, rect.height))
+                .unwrap_or(Vec2::ZERO)
         } else {
             self.size
         }
+    }
+
+    pub fn get_size_info(&self, request_size: Vec2, rect2d: Option<&Rect2D>) -> (Vec2, Vec2) {
+        let mut fixed_size = self.get_size(rect2d);
+
+        if fixed_size.x < 0f32 {
+            if self.hor == LayoutAlignment::Stretch {
+                fixed_size.x = request_size.x - self.margin.horizontal();
+            }
+        }
+        if fixed_size.y < 0f32 {
+            if self.ver == LayoutAlignment::Stretch {
+                fixed_size.y = request_size.y - self.margin.vertical();
+            }
+        }
+        let free_inner_size = Vec2::new(
+            if fixed_size.x < 0f32 {
+                request_size.x - self.margin.horizontal() - self.padding.horizontal()
+            } else {
+                fixed_size.x - self.padding.horizontal()
+            },
+            if fixed_size.y < 0f32 {
+                request_size.y - self.margin.vertical() - self.padding.vertical()
+            } else {
+                fixed_size.y - self.padding.horizontal()
+            },
+        );
+
+        (fixed_size, free_inner_size)
     }
 }
 
 pub enum TypeElement {
     Stack(StackLayout),
     ViewBox,
-    View
+    View,
 }
 
 #[derive(Component)]
 pub struct LayoutElement {
-    pub common:CommonView,
-    pub typ_elem:TypeElement,
+    pub common: CommonView,
+    pub typ_elem: TypeElement,
 }
 
-
 impl LayoutElement {
-    pub fn create_stack(spacing:f32,orientation:Orientation) -> LayoutElement {
+    pub fn create_stack(spacing: f32, orientation: Orientation) -> LayoutElement {
         LayoutElement {
-            common:CommonView::default(),
-            typ_elem:TypeElement::Stack(StackLayout {spacing,orientation})
+            common: CommonView::default(),
+            typ_elem: TypeElement::Stack(StackLayout {
+                spacing,
+                orientation,
+            }),
         }
     }
-    
-    pub fn is_invalid_measure(&self,child:&LayoutElement) -> bool {
+
+    pub fn is_invalid_measure(&self, child: &LayoutElement) -> bool {
         //TODO 判断布局是否失效
         true
     }
-
-    
 }
