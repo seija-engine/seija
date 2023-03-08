10a3c360ba873426b48edbc0d9ad89335e951784
yangdao <458424869@qq.com>
2023-03-08 20:23:31 +0800

    flex wrap arrange

--- crates/seija-ui/src/layout/arrange.rs
+++ crates/seija-ui/src/layout/arrange.rs
@@ -159,7 +159,8 @@ fn arrange_flex_element(
 ) -> Vec2 {
     match flex.warp {
         FlexWrap::Wrap => {
-            arrange_flex_element_wrap(entity, flex, element, parent_origin, parent_size, params)
+            Vec2::ZERO
+            //arrange_flex_element_wrap(entity, flex, element, parent_origin, parent_size, params)
         }
         FlexWrap::NoWrap => {
             arrange_flex_element_nowrap(entity, flex, element, parent_origin, parent_size, params)
@@ -188,6 +189,10 @@ impl<'a> FlexIter<'a> {
             },
         }
     }
+
+    fn child_count(&self) -> usize {
+        self.children.map(|v| v.len()).unwrap_or(0)
+    }
 }
 
 impl<'a> Iterator for FlexIter<'a> {
@@ -209,6 +214,8 @@ impl<'a> Iterator for FlexIter<'a> {
             None
         }
     }
+
+   
 }
 
 fn arrange_flex_element_nowrap(entity: Entity,flex: &FlexLayout,element: &LayoutElement,parent_origin: Vec2,parent_size: Vec2,params: &LayoutParams) -> Vec2 {
@@ -259,9 +266,9 @@ fn arrange_flex_element_nowrap(entity: Entity,flex: &FlexLayout,element: &Layout
         }
 
         //分散对齐,两边贴靠
-        FlexJustify::SpaceBetween => { arrange_flex_spacebetween(entity,lt_pos,flex,params) }
+        FlexJustify::SpaceBetween => { arrange_flex_spacebetween(entity,lt_pos,flex,this_axis_size,inner_size,params) }
         //分散对齐，两边是中间的一半
-        FlexJustify::SpaceAround => {}
+        FlexJustify::SpaceAround => { arrange_flex_spacearound(entity,lt_pos,flex,this_axis_size,inner_size,params) }
         _ => {}
     }
 
@@ -308,22 +315,85 @@ fn arrange_by_start_pos(start_pos:Vec2,flex: &FlexLayout,entity: Entity,inner_si
     }
 }
 
-fn arrange_flex_spacebetween(entity: Entity,lt_pos:Vec2,flex:&FlexLayout,params: &LayoutParams) {
+fn arrange_flex_spacebetween(entity: Entity,lt_pos:Vec2,flex:&FlexLayout,this_axis_size:Vec2,this_size:Vec2,params: &LayoutParams) {
     let flex_iter = FlexIter::new(params.childrens.get(entity).ok(), flex.direction);
+    let child_count = flex_iter.child_count();
+    let mut main_size_sum:f32 = 0f32;
+    let spacing = this_axis_size.x / (child_count - 1) as f32;
     for (index,child_entity) in flex_iter.enumerate() {
         let child_elem = params.elems.get(child_entity).unwrap_or(&VIEW_ID);
         let child_rect = params.rect2ds.get(child_entity).unwrap_or(&RECT2D_ID);
+        let mut pos;
         if index == 0 {
-           
-        } else if index == 1 {
-
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + child_rect.width * 0.5f32,lt_pos.y); 
+                main_size_sum += child_rect.width;
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - child_rect.height * 0.5f32);
+                main_size_sum += child_rect.height;
+            }
+        } else if child_count == index + 1 {
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + this_axis_size.x - child_rect.width * 0.5f32,lt_pos.y); 
+                
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - this_axis_size.x + child_rect.height * 0.5f32);
+            }
         } else {
-
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + main_size_sum + spacing * index as f32,lt_pos.y); 
+                main_size_sum += child_rect.width;
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - main_size_sum - spacing * index as f32);
+                main_size_sum += child_rect.height;
+            }
         }
+        if flex.is_hor() {
+            pos.y = align_flex_cross(flex.align_items, lt_pos.y, this_axis_size.x, child_rect.height, 1f32);
+        } else {
+            pos.x = align_flex_cross(flex.align_items, lt_pos.x, this_axis_size.x, child_rect.width, -1f32);
+        }
+        arrange_layout_element(child_entity,child_elem,pos,this_size,ArrangeXY::NONE,params);
     }
 }
 
-
-fn arrange_flex_element_wrap(entity: Entity,flex: &FlexLayout,element: &LayoutElement,parent_origin: Vec2,parent_size: Vec2,params: &LayoutParams) -> Vec2 {
-    Vec2::ZERO
+fn arrange_flex_spacearound(entity: Entity,lt_pos:Vec2,flex:&FlexLayout,this_axis_size:Vec2,this_size:Vec2,params: &LayoutParams) {
+    let flex_iter = FlexIter::new(params.childrens.get(entity).ok(), flex.direction);
+    let child_count = flex_iter.child_count();
+    let mut main_size_sum:f32 = 0f32;
+    let spacing = this_axis_size.x / child_count as f32;
+    for (index,child_entity) in flex_iter.enumerate() {
+        let child_elem = params.elems.get(child_entity).unwrap_or(&VIEW_ID);
+        let child_rect = params.rect2ds.get(child_entity).unwrap_or(&RECT2D_ID);
+        let mut pos;
+        if index == 0 {
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + child_rect.width * 0.5f32 + spacing * 0.5f32,lt_pos.y); 
+                main_size_sum += child_rect.width + spacing * 0.5f32;
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - child_rect.height * 0.5f32 - spacing * 0.5f32);
+                main_size_sum += child_rect.height + spacing * 0.5f32;
+            }
+        } else if child_count == index + 1 {
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + this_axis_size.x - child_rect.width * 0.5f32 - spacing * 0.5f32,lt_pos.y); 
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - this_axis_size.x + child_rect.height * 0.5f32 + spacing * 0.5f32);
+            }
+        } else {
+            if flex.is_hor() { 
+                pos = Vec2::new(lt_pos.x + main_size_sum + spacing * index as f32,lt_pos.y); 
+                main_size_sum += child_rect.width;
+            } else {  
+                pos = Vec2::new(lt_pos.x,lt_pos.y - main_size_sum - spacing * index as f32);
+                main_size_sum += child_rect.height;
+            }
+        }
+        if flex.is_hor() {
+            pos.y = align_flex_cross(flex.align_items, lt_pos.y, this_axis_size.x, child_rect.height, 1f32);
+        } else {
+            pos.x = align_flex_cross(flex.align_items, lt_pos.x, this_axis_size.x, child_rect.width, -1f32);
+        }
+        arrange_layout_element(child_entity,child_elem,pos,this_size,ArrangeXY::NONE,params);
+    }
 }
